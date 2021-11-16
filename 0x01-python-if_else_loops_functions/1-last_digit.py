bfd6847261e8e707f80be79c3b5a9838fa8eb8d8
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-11-16 12:04:21 -0800

    assign a random signed number and print the last digit of the number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,10 @@ if number < 0:
     last = -last
 else:
     last = last
+print("Last digit of {} is {} and is ".format(number, last), end="")
 if last > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last), end="")
+    print("greater than 5")
 elif last == 0:
-    print("Last digit of {} is {} and is 0".format(number, last), end="")
-elif last < 6 and last != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last), end="")
+    print("0")
+else:
+    print("less than 6 and not 0")
