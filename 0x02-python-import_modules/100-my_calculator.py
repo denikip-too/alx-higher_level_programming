6ed6b561cf1a36edbcf085f35569a5943a2642fc
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-11-18 11:27:05 -0800

    imports all functions from the file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    from calculator_1 import *
+    from calculator_1 import add, sub, mul, div
     argv = sys.argv[1:]
     i = 1
     sum = 0
