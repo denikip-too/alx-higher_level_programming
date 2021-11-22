137de25a30288147149f2b03f79690ab30a7c8f3
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-11-22 15:05:11 -0800

    adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+    res = [0, 0]
+    for i in range(2):
+        res[i] += tuple_a[i] if len_a > i else 0
+        res[i] += tuple_b[i] if len_b > i else 0
+    return tuple(res)
+
