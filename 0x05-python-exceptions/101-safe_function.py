5955861120b9f0847eeed4ed994bcf7edc394eae
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-12-06 18:47:09 -0800

    executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return (result)
     except:
-        return (None)
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
