43e374865641306af755fca678dc77f796d7d03d
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-12-17 02:43:42 -0800

    defines a square and instatiate

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Square"""
+
+
+class Square:
+    """defines a square and Instantiate"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")            
+        elif size < 0:
+            raise ValueError("size must be >= 0")
