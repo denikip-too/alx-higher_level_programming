c05085f7f45bb32137282a69313644da43816423
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-12-21 13:15:48 -0800

    reads a text file and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Read file"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open("my_file_0.txt", encoding="utf-8") as filename:
+        for i in filename:
+            print(i, end="")
