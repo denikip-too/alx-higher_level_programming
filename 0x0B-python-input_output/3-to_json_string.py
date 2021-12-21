b241ead9d5d3ad4c1dc5bc14e8fbb35f8ea650a9
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-12-21 13:23:37 -0800

    returns the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import Module"""
+import json
+
+
+"""To JSON string"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return json.dumps(my_obj)
