619705900a4be122c3c6c0eeed9a3801577cf5d3
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-12-22 14:41:11 -0800

    inserts a line of text to a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,7 +5,7 @@
 def append_after(filename="", search_string="", new_string=""):
     """inserts a line of text to a file"""
     with open(filename, "r+") as f:
-    contents = f.readlines()
+        contents = f.readlines()
 
     with open(filename, "w") as f:
         i = 0
