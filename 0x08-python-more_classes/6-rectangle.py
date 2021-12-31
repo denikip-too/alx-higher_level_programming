e7a644cd4aaa8c3f4599f4040573a3573f5c5e69
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-12-31 13:18:52 -0800

    fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """definition of a rectangle"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
