e8f4e7e0cb8b003c4b4a9cf075df97022b55e098
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-05 13:22:20 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,7 +50,7 @@ class Base:
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
         if json_string is None:
-            return "[]"
+            return ("[]")
         else:
             return json.loads(json_string)
 
