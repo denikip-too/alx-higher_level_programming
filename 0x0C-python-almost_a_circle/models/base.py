f7ad23190b8765416f5585cc4a90f9615851b958
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-06 13:38:14 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -63,6 +63,8 @@ class Base:
         with open(filename, 'r') as f:
             file_string = f.read().replace('\n', '')
             data = cls.from_json_string(file_string)
+            if data is None:
+                return []
             for el in data:
                 if type(el) == dict:
                     object_created.append(cls.create(**el))
