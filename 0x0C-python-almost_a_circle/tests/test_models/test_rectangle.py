5c86d25af948223fb5f9f156b1d831536e42a0d7
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-01-09 04:50:29 -0800

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,6 +18,9 @@ class TestRectangle(unittest.TestCase):
 
     def test_attribute_validation(self):
         """attribute validation testing"""
+        r8 = Rectangle(10, 2, 4, 5, 1)
+        r9 = Rectangle(2, 10, 5, 7, 2)
+
         with self.assertRaises(TypeError):
             r1 = Rectangle(10, "2")
             r2 = Rectangle(10, 2, {}, 3)
@@ -37,6 +40,14 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.area(), 6)
         self.assertEqual(r2.area(), 20)
         self.assertEqual(r3.area(), 56)
+
+    """def test_display(self):
+        r1 = Rectangle(4, 6)
+        d1 = '####\n####\n####\n####\n####\n####'
+        r2 = Rectangle(2, 2)
+        d2 = '####\n####'
+        self.assertEqual(r1.display(), d1)
+        self.assertSequenceEqual(r2.display(), d2)"""
 
     def test_str(self):
         """tests for attributes of Rectangle"""
