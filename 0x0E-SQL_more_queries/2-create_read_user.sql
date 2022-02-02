cbf0bfec69aee7465dbaa6d222ac0100d3f8822e
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-02 09:45:06 -0800

    creates the database and the user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- creates the database if only exist and the user if only exist and set password. user should have only SELECT privilege in the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
