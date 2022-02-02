84fa376cea535aacafdc63ec5601c91c54ab3a10
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-02 12:22:09 -0800

    lists all the cities of California that can be found in the database

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- lists all the cities of California that can be found in the database hbtn_0d_usa
+SELECT FROM states.name, cities.name where cities.state_id = states.id, cities.name = California, cities.name = California;
