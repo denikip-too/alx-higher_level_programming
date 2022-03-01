a39d463e47c15c07901a5f447446528ecf5a50f3
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 22:58:06 -0800

    prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//prints the first argument passed to it
+// prints the first argument passed to it
 
 if (process.argv[2]) {
-	console.log(process.argv[2])
-}
-else {
-	console.log('No argument');
+  console.log(process.argv[2])
+} else {
+  console.log('No argument');
 }
