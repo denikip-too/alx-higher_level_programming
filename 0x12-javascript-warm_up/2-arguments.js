7a6cd3252605f12eee6de239008f37822a9f2cb7
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 22:52:30 -0800

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-//prints a message depending of the number of arguments passed
+// prints a message depending of the number of arguments passed
 
-if (process.argv.length === 0) {
-	console.log('No argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
 }
-else if (process.argv.length === 1) {
-	console.log('Argument found');
+else if (process.argv.length === 3) {
+  console.log('Argument found');
 }
 else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
