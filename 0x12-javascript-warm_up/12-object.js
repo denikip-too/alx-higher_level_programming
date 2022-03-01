60dbb4c58ee553b9476317e7d08ea54b5133fa65
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 23:48:05 -0800

    fix

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-//replace the value 12 with 89
+// replace the value 12 with 89
 
 const myObject = {
-	type: 'object',
-	value: 12
+  type: 'object',
+  value: 12
 };
 console.log(myObject);
 myObject.value = 89;
