ad73ca2cc3648fb911c59031274b3d3548d8574f
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 23:21:45 -0800

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-//prints x times “C is fun”
+// prints x times “C is fun”
 
 let x = parseInt(process.argv[2]);
 if (x) {
-	for (let i = 1; i <= x; i++)
-	console.log('C is fun');
-}
-else {
-	console.log('Missing number of occurrences');
+  for (let i = 1; i <= x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
