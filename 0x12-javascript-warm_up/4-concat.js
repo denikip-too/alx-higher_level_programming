ce42681d9512adb8a898c2323321a00498e91acc
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 23:07:12 -0800

    prints two arguments passed to it

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-//prints two arguments passed to it
+// prints two arguments passed to it
 
 if (!(process.argv[3]) && process.argv[2]) {
 	console.log(process.argv[2] + ' is undefined');
-}
-else if (process.argv[2] && process.argv[3]) {
+} else if (process.argv[2] && process.argv[3]) {
 	console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
-else {
+} else {
 	console.log('undefined is undefined');
 }
