f4d50e2bc5e4f05369c442a4a9d78660957c5e1f
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 16:18:57 -0800

    prints a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+//prints a message depending of the number of arguments passed
+
+if (process.argv.length === 0) {
+	console.log('No argument');
+}
+else if (process.argv.length === 1) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
