55c98a2afef9abb9c7745441bdc2dc446dbe84a3
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 23:15:31 -0800

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//prints if the first argument can be converted to an integer
+// prints if the first argument can be converted to an integer
 
 if (parseInt(process.argv[2])) {
-	console.log('My number: ' + parseInt(process.argv[2]));
-}
-else {
-	console.log('Not a number');
+  console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
 }
