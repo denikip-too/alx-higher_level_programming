ddd14ac2892149d410185c405a7407e0273e6f3d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-02-28 23:23:49 -0800

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints x times “C is fun”
 
-let x = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 if (x) {
   for (let i = 1; i <= x; i++) {
     console.log('C is fun');
