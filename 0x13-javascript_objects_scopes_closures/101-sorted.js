efcdb96d901623add5bbc6f2e23e3aa80347d29a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-01 17:15:00 -0800

    fix

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* imports a dictionary of occurrences by user id and computes a dictionary of user
+ids by occurrence
+- A key is a number of occurrences
+- A value is the list of user ids
+*/
+const dct = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dct) {
+  if (newDict[dct[key]] === 'undefined') {
+    newDict[dct[key]] = [];
+  }
+  newDict[dct[key]].push(key);
+}
+console.log(newDict);
