f9ef3da681024e3a62629f3a6307e7eec004d305
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-01 16:50:43 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // prints the number of arguments already printed and the new argument value
 
+let count = 0;
 exports.logMe = function (item) {
-  let count = 0;
   console.log(count + ': ' + item);
   count++;
 };
