479b1e96fc88403daabbd69e41b46574ddd4cba2
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-10 03:48:08 -0800

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""import modules"""
+import MySQLdb
+import sys
+
+
+def get_states():
+    """lists all states from the database hbtn_0e_0_usa"""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    get_states()
