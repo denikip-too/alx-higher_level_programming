4f5fbaa78c2afc61667986c2660085863c27d8a2
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-11 11:49:21 -0800

    lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,6 +7,7 @@ from model_state import Base, State
 
 
 def list_state_obj():
+    """lists all State objects from the database hbtn_0e_6_usa"""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
@@ -20,6 +21,7 @@ def list_state_obj():
         print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
 
     session.close()
+
 
 if __name__ == "__main__":
     list_state_obj()
