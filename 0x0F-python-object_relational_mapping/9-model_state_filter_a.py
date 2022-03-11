3a719712313f40356dcec2e6dc7dc3e44c74d75e
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-11 12:17:55 -0800

    lists all State objects that contain the letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,6 +7,7 @@ from model_state import Base, State
 
 
 def list_a_state_obj():
+    """lists all State objects that contain the letter a"""
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
@@ -21,6 +22,7 @@ def list_a_state_obj():
             print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
 
     session.close()
+
 
 if __name__ == "__main__":
     list_a_state_obj()
