5fdc5b48ab1bcb8b52dbcc3c7bda7669e237ba9d
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-20 15:01:53 -0700

    cURL a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends JSON POST request to URL passed as first argument and display response
-curl -sX POST -H "Content-Type: application/json" "$1"
+curl -sX POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@$2" "$1"
