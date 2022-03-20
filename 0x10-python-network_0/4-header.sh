2307ed38ed73b5c285330d037dd0a654d098ef40
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-20 14:49:43 -0700

    sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a URL as an argument, sends a GET request to URL and displays response
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sX GET -H "X-School-User-Id: 98" "$1"
