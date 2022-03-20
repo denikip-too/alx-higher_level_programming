2c99cae999c10ce7935a7e095bca6a4dc1ca8193
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-20 14:41:52 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the
-#size of the body of the response in bytes
+# takes in URL, sends a request to that URL, and displays size of the response
 curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f 2
