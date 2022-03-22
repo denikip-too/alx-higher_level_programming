56e564711a864826126998449e14066d7f33e5c0
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 15:22:09 -0700

    list 10 commits (from the most recent to oldest) of the repository by the user

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest) of the repository
+    and by the user"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
+    result = requests.get(url)
+    try:
+        d = result.json()
+        for i in range(10):
+            print("{}: {}".format(d[i]["sha"],
+                                  d[i]["commit"]["author"]["name"]))
+    except IndexError:
+        pass
