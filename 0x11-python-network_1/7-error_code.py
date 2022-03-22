9dbf2888083cd3cd79081c621fb4b8e80666d66e
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 13:51:25 -0700

    displays Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" displays Error code"""
+import requests
+import sys
+
+
+def error_code():
+    """displays Error code"""
+    r = requests.get(sys.argv[1])
+    try:
+        if r.status_code >= 400:
+            print("Error code: ", r.status_code)
+        else:
+            print(r.content)
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    error_code()
