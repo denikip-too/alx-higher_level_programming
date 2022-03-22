aeef9ddc09679867c98441dd5d1f5ee5e5130116
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 11:42:11 -0700

    display status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-    f = resp.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t-content: {}".format(f.text))
+        f = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r.text)))
+        print("\t-content: {}".format(f.text))
