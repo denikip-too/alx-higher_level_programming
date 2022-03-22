a63ea36ecffd3a3b877fa7256d20afe1f93f1a4e
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 13:55:03 -0700

    displays Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ def error_code():
     r = requests.get(sys.argv[1])
     try:
         if r.status_code >= 400:
-            print("Error code: ", r.status_code)
+            print("Error code: {}".format(r.status_code))
         else:
             print(r.content)
     except:
