34883d60ce4af71259fe8f74e6ebe73eb3c2ccda
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 15:00:55 -0700

    displays Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
             f = response.read()
             print(f.decode("utf-8"))
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.status_code))
+        print("Error code: {}".format(e.code))
