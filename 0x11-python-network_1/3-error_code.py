4bbf7834c47c95d89036a8396c30648091d3333c
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 13:38:46 -0700

    displays Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""sends a request to the URL and displays the body of the response """
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            f = response.read()
+            print(f.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: ", e.code)
