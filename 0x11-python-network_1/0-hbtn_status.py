ef119e05010a117e724db3269fef8d72c413da71
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-03-22 11:38:39 -0700

    display status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""display my status"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
+    f = resp.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t-content: {}".format(f.text))
