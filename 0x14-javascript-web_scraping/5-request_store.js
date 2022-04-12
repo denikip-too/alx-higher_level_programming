c09686e489cd62607c385edbb40cfba465df251a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-12 14:43:41 -0700

    script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 // JS script that gets the contents of a webpage and stores it in a file
-let request = require('request');
-let fs = require('fs');
-let url = process.argv[2];
-let file = process.argv[3];
+const request = require('request')
+const fs = require('fs')
+const url = process.argv[2]
+const file = process.argv[3]
 request.get(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.log(error)
   } else {
-    fs.writeFile(file, body , function (error) {
+    fs.writeFile(file, body, function (error) {
       if (error) {
-        console.log(error);
+        console.log(error)
       }
-    });
+    })
   }
-});
+})
