cb8639200c9d247c3afd1d8f6453fd9cd91561d8
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-12 15:19:10 -0700

    script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Js script that writes a string to a file
-let fs = require('fs');
+const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], (err) => {
   if (err) {
     console.log(err);
