5886d3199fb01a2c0bec5a6eba45b0479b5d7210
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-13 16:39:46 -0700

    script that prints the number of movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 // script that prints the number of movies where the character “Wedge Antilles”
 // is present
-let request = require('request');
-let url = process.argv[2];
-request.get(url + '/18/', function(error, response, body) {
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let temp = JSON.parse(body);
-    let j = 0;
-    for (let i = 0; i < temp.length; i++) {
-      j++;
-    }
-    console.log(j);
+    const temp = JSON.parse(body);
+    console.log(temp.reduce((j, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? j + 1
+        : j;
+    }, 0));
   }
 });
