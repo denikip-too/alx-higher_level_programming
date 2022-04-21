f57c6a90973049f1220a1e987d055d40fe3bd24a
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-21 16:20:31 -0700

    updates the text color of the <header> element

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// updates the text color of the <header> element to red (#FF0000)
+
+document.addEventListener('DOMContentLoaded', () => {
+  const c = document.querySelector('header');
+  c.style.color = ('#FF0000');
+});
