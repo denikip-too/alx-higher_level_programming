5ae520e92572b9619086649d646dd071e2636b0c
kiprotich-denis <kiprotichdenis95@gmail.com>
2022-04-21 10:12:21 -0700

    fetches and lists the title for all movies by using a URL

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+// fetches and lists the title for all movies by using a URL using JQuery API
+
+const title = $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  for (let i = 0; i < data.results.length; i++) {
+    $('UL#list_movies').append('<li>' + data.results[i].title + '</li>');
+  }
+});
